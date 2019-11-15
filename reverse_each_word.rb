8bcbcbc9b897ffd2abf4dc2171d04ebadc20f798
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-15 01:51:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def reverse_each_word(thing)
   new_array = thing.split(" ")
   new_array.each {|words| words.reverse!}.join(" ")
@@ -8,4 +9,10 @@ def reverse_each_word(thing)
   new_array = array.split(" ")
   reversed_array = new_array.collect {|x| x.reverse!}
   reversed_array.join(" ")
+=======
+def reverse_each_word (string)
+  new_array=string.split('')
+  
+  new_array.each
+>>>>>>> 70a7d93e86332235dbb0afdda5744ced3e008024
 end
